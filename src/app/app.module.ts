52bf76922bc05c6233b5c8226d3f56e5570c9d32
@nikita <angdev329@gmail.com>
2023-07-10 13:30:34 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardSettingsComponent } from './card-settings/card-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardSettingsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/card-settings/card-settings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-settings',
+  templateUrl: './card-settings.component.html',
+  styleUrls: ['./card-settings.component.scss']
+})
+export class CardSettingsComponent {
+
+}
