b2afd2584fa388701f94ab40969494b46d545fc0
@nikita <angdev329@gmail.com>
2023-07-28 15:06:05 +0300

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,12 +25,12 @@ export class AppComponent {
 
       name: ['Card Name'],
       categories: ['Human'],
-      description: ['<b>Stat:</b> Dexterity'],
+      description: ['ㅤ<b>Stat:</b> Dexterity\n' +
+      'Additional text.'],
       keywords: ['<center><b>Keyword1</b>, <b>Keyword2</b></center>'],
       flavourText: ['<i>Up for a round of Gwent?</i>']
     });
-    // TODO: empty symbol (ㅤ)
-
+    
     // Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   }
 
