0f890464dac8398ef3f1a1d7bd5d1623304823d6
@nikita <angdev329@gmail.com>
2023-07-30 19:25:09 +0300

    provision field and new app icon

---
+++ src/app/_pipes/get-provision-banner-image.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GetProvisionBannerImagePipe } from './get-provision-banner-image.pipe';
+
+describe('GetProvisionBannerImagePipe', () => {
+  it('create an instance', () => {
+    const pipe = new GetProvisionBannerImagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
