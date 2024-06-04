68de27a9ce77bd0ff1d9c724df5152d5c3e98882
@nikita <angdev329@gmail.com>
2024-06-04 14:54:35 +0300

    attempt to understand this piece of strange library (quill)

--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -1,13 +1,15 @@
 import {Component, Input} from '@angular/core';
 import {QuillEditorComponent} from "ngx-quill";
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-text-editor',
   standalone: true,
   imports: [
     QuillEditorComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgOptimizedImage
   ],
   templateUrl: './text-editor.component.html',
   styleUrl: './text-editor.component.scss'
@@ -19,13 +21,15 @@ export class TextEditorComponent {
 
   getEditorInstance(editorInstance: any) {
     this.quillEditorRef = editorInstance;
-    const toolbar = this.quillEditorRef.getModule('toolbar');
-    toolbar.addHandler('clean', this.cleanController);
+    /* TODO: indent handler is needed
+        const toolbar = this.quillEditorRef.getModule('toolbar');
+        toolbar.addHandler('clear', this.clearController);
+    */
   }
 
-  cleanController = () => {
+  clear() {
     this.quillEditorRef.removeFormat(0, this.quillEditorRef.getLength(), 'user');
-    this.quillEditorRef.deleteText(0, this.quillEditorRef.getLength() - 1, 'user');
+    this.quillEditorRef.deleteText(0, this.quillEditorRef.getLength(), 'user');
   }
 
 }
