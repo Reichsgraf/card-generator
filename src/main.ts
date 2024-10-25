2fee34b5f288672b777f7add9f6677a884ef73fb
@nikita <angdev329@gmail.com>
2024-10-25 13:15:15 +0300

    module refactoring

--- src/main.ts
+++ src/main.ts
@@ -5,32 +5,47 @@ import { QuillEditorToolbar } from './app/_utility/quill-rich-text-editor';
 import { QuillModule, QuillConfigModule } from 'ngx-quill';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgxTranslateModule } from './app/_modules/translate/translate.module';
 import { NgOptimizedImage } from '@angular/common';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { ResponsiveService } from './app/_modules/responsive/responsive.service';
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient, provideHttpClient} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http,
+    './assets/i18n/',
+    '.json');
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(
-          BrowserModule,
-          NgOptimizedImage,
-          NgxTranslateModule,
-          ReactiveFormsModule,
-          ClipboardModule,
-          QuillModule.forRoot(),
-          QuillConfigModule.forRoot({
-            modules: {
-                syntax: false,
-                toolbar: {
-                    container: QuillEditorToolbar,
-                }
-            },
-          }),
-          MatTooltipModule
-        ),
-        ResponsiveService
-    ]
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(
+      BrowserModule,
+      NgOptimizedImage,
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+      ReactiveFormsModule,
+      ClipboardModule,
+      QuillModule.forRoot(),
+      QuillConfigModule.forRoot({
+        modules: {
+          syntax: false,
+          toolbar: {
+            container: QuillEditorToolbar,
+          }
+        },
+      }),
+      MatTooltipModule
+    ),
+    ResponsiveService
+  ]
 })
   .catch(err => console.error(err));
